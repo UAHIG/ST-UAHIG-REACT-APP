e1d39d72e02530ca48027f0c06474d3b41f9d681
UaHig <golosnyyigor@gmail.com>
2024-02-14 18:26:19 +0100

    changed package files

--- 07-create-react-app-eject/src/App.js
+++ 07-create-react-app-eject/src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <h1>Hello from Create React APP</h1>
+        <h1>Hello from Create React APP!!!</h1>
 
         <a
           className='App-link'
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-         Learn React
+          Learn React
         </a>
       </header>
     </div>
