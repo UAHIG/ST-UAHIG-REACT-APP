508dee942d7f3ab96ac2778fac456d9b32a7bc61
UaHig <golosnyyigor@gmail.com>
2024-02-21 15:51:29 +0100

    feat: lesson 124 finished

---
+++ 10-conditional-rendering/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css"
+import PetInfo from "./components/Petinfo"
+
+function App() {
+  return <div className='App'>
+    <PetInfo animal='cat' age="55" hasPet />
+    <PetInfo animal='giraf' age="5" hasPet={false} />
+    <PetInfo animal='rat' age="1" hasPet={false} />
+  </div>
+}
+
+export default App
+
