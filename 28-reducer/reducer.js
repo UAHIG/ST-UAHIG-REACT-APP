ad91b9780af69498c0a91b9bbf524d01fec62849
UaHig <golosnyyigor@gmail.com>
2024-03-14 12:56:45 +0100

    feat: lesson 311  end

---
+++ 28-reducer/reducer.js
@@ -0,0 +1,72 @@
+const iniialState = []
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_NAME":
+      return [...state, action.payload]
+    case "DELETE_NAME":
+      return state.filter((personName) => personName !== action.payload)
+    case "CLEAR_NAMES":
+      return []
+    default:
+      return state
+  }
+
+  // if (action.type === "ADD_NAME") {
+  //   return [...state, action.payload]
+  // }
+  // if (action.type === "DELETE_NAME") {
+  //   return state.filter((personName) => personName !== action.payload)
+  // }
+  // if (action.type === "CLEAR_NAMES") {
+  //   return []
+  // }
+  // return state
+}
+
+let newState = reducer(iniialState, {
+  type: "ADD_NAME",
+  payload: "Bogdan",
+})
+
+newState = reducer(newState, {
+  type: "ADD_NAME",
+  payload: "Alice",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "ADD_NAME",
+  payload: "Alice",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "DELETE_NAME",
+  payload: "Bogdan",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "CLEAR_NAMES",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "ADD_NAME",
+  payload: "Alice",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "ADD_NAME",
+  payload: "Alice",
+})
+console.log(newState)
+
+newState = reducer(newState, {
+  type: "ADD_NAME",
+  payload: "Alice",
+})
+
+console.log(newState)
