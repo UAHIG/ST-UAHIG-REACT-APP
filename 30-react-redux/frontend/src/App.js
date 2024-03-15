b2a7682f899616b19d7112ef8c6f29cec6b28583
UaHig <golosnyyigor@gmail.com>
2024-03-15 13:03:32 +0100

    feat: commit after err

--- 30-react-redux/frontend/src/App.js
+++ 30-react-redux/frontend/src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
-    <div className="app">
-      <h1>Book Library App</h1>
+    <div className='app'>
+      <header className='app-header'>
+        <h1>Book Library App</h1>
+      </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
